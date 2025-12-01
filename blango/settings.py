1a50ecb09e8f2e8db209165e67e707f0f245cfe4
Salman Salman <msalmanimran780@gmail.com>
2025-12-01 02:06:31 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -181,3 +181,5 @@ class Dev(Configuration):
   CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
   CRISPY_TEMPLATE_PACK = "bootstrap5"
 
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+  ACCOUNT_ACTIVATION_DAYS = 7
--- blango/urls.py
+++ blango/urls.py
@@ -1,34 +1,41 @@
-import blog.views
-
-"""blango URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-import debug_toolbar
-from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+import debug_toolbar
 
 import blog.views
+import blango_auth.views
+from django_registration.backends.activation.views import RegistrationView
+from blango_auth.forms import BlangoRegistrationForm
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", blog.views.index),
+
+    path("", blog.views.index, name="home"),
+
+    # MUST BE FIRST – override the default register URL
+    path(
+        "accounts/register/",
+        RegistrationView.as_view(form_class=BlangoRegistrationForm),
+        name="django_registration_register",
+    ),
+
+    # Django built-in auth URLs
+    path("accounts/", include("django.contrib.auth.urls")),
+
+    # django-registration URLs (activation workflow)
+    path("accounts/", include("django_registration.backends.activation.urls")),
+
+    # profile
+    path("accounts/profile/", blango_auth.views.profile, name="profile"),
+
+    # blog
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
+
     path("ip/", blog.views.get_ip),
-    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
+
 
 if settings.DEBUG:
     urlpatterns += [
